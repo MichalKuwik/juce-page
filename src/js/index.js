aa5cb3b383679c172ac8f9ce432959e3b683d87b
Michał Kuwik <Michal7074@poczta.fm>
2019-07-03 20:03:49 +0200

    edit for sm devices RWD

--- src/js/index.js
+++ src/js/index.js
@@ -2,15 +2,21 @@
 //get dom el
 const menuBurger = document.querySelector('.menu-hamburger');
 const menuLines = document.querySelectorAll('.menu-hamburger div');
+const menu = document.querySelector('.menu-mobile');
+const menuItems = document.querySelectorAll('.menu-item');
 let activeMenu = false;
 
 const handleClickMenu = () => {
   if(!activeMenu){
     menuLines.forEach(item => item.classList.add('active'));
+    menu.classList.add('show');
+    menuItems.forEach(element => element.classList.add('showItem'));
     activeMenu = true;
     console.log('click');
   }else{
     menuLines.forEach(item => item.classList.remove('active'));
+    menu.classList.remove('show');
+    menuItems.forEach(element => element.classList.remove('showItem'));
     activeMenu = false;
   }
   
