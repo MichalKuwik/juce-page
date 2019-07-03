dd8529f971108980760b53a9aea3d7b575ea153e
Michał Kuwik <Michal7074@poczta.fm>
2019-07-03 19:04:15 +0200

    create two files _menu.scss,_config.scss and config with them media queries, create menu hamburger and alive them in js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
-console.log('hello');
+
+//get dom el
+const menuBurger = document.querySelector('.menu-hamburger');
+const menuLines = document.querySelectorAll('.menu-hamburger div');
+let activeMenu = false;
+
+const handleClickMenu = () => {
+  if(!activeMenu){
+    menuLines.forEach(item => item.classList.add('active'));
+    activeMenu = true;
+    console.log('click');
+  }else{
+    menuLines.forEach(item => item.classList.remove('active'));
+    activeMenu = false;
+  }
+  
+}
+
+menuBurger.addEventListener('click',handleClickMenu);
