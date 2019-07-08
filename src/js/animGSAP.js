13b96d54c140bf110bc86a219ce5dd886892e625
Michał Kuwik <Michal7074@poczta.fm>
2019-07-08 15:54:27 +0200

    add tweenMax library to strawbery.html and add animation for few elements on this page

--- src/js/animGSAP.js
+++ src/js/animGSAP.js
@@ -1,6 +1,7 @@
 window.onload = load;
 
 function load(){
+  //index.html
   TweenMax.from('.div-branch', 1,//speed 
   { scale: 0, 
     height: '100vh',
@@ -11,5 +12,17 @@ function load(){
     height: '100vh',
     ease: Back.easeOut.config(.7),
   });
+  // strawbery.html
+  TweenMax.from('.right-content-red', 1,//speed 
+  { height:0,
+    ease: Back.easeOut.config(1.9),
+  });
+  TweenMax.from('.overlay-red', 1,//speed 
+  { opacity:0,
+    delay:0.4,
+    ease: Back.easeOut.config(1.9),
+  });
+
+ 
 
 }
